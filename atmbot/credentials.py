35861fa275de625c7df501a4590a2fafe6710d42
fgiannotti <franco.giannotti@mercadolibre.com>
2021-03-20 16:01:42 -0300

    add credentials

--- atmbot/credentials.py
+++ atmbot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "here goes your access token from BotFather"
-bot_user_name = "the username you entered"
+bot_token = "1767860663:AAEtgTvk7nqLw9DBxzB9cqsEKnQV8zYzBqs"
+bot_user_name = "AtmFinder"
 URL = " https://atm-bot-arg.herokuapp.com/"
