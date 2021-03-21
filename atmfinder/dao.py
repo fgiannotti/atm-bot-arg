7315832ff625b9213ee5fcb112a819248aada99a
fgiannotti <franco.giannotti@mercadolibre.com>
2021-03-21 15:05:32 -0300

    dao.py fixes

--- atmfinder/dao.py
+++ atmfinder/dao.py
@@ -1,7 +1,7 @@
-import models
+from atmfinder.models import ATM
 import psycopg2
 from psycopg2 import sql
-def SearchByNetwork(atm: models.ATM = None):
+def SearchByNetwork(atm: ATM = None):
     atm = []
     try:
         db = psycopg2.connect(dbname="dehk8k9ckm1b5r", user="ulnxxperftvpyj", password="9d6f464e63dc1b36466508b5a2d1c2b8cb172e20bc29677fd4556940491b7dc3", host="ec2-18-214-208-89.compute-1.amazonaws.com")
