07c8c0ad6fc852aa034aed68bf48e924636c6a20
fgiannotti <franco.giannotti@mercadolibre.com>
2021-03-20 21:58:11 -0300

    add postgre req

--- atmfinder/dao.py
+++ atmfinder/dao.py
@@ -1,7 +1,7 @@
-import psycopg
+import psycopg2
 
 # Connect to your postgres DB
-conn = psycopg.connect("dbname=test user=postgres")
+conn = psycopg2.connect(dbname="dehk8k9ckm1b5r", user="ulnxxperftvpyj", password="9d6f464e63dc1b36466508b5a2d1c2b8cb172e20bc29677fd4556940491b7dc3", host="ec2-18-214-208-89.compute-1.amazonaws.com")
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
