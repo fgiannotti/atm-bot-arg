d5be0d27ff5f956652c2204387737711f1706ba6
fgiannotti <franco.giannotti@mercadolibre.com>
2021-03-21 13:40:16 -0300

    logs

--- atmfinder/dao.py
+++ atmfinder/dao.py
@@ -7,7 +7,7 @@ def SearchAll():
 	try:
 		db = psycopg2.connect(dbname="dehk8k9ckm1b5r", user="ulnxxperftvpyj", password="9d6f464e63dc1b36466508b5a2d1c2b8cb172e20bc29677fd4556940491b7dc3", host="ec2-18-214-208-89.compute-1.amazonaws.com")
 		cursor = db.cursor()
-		query = "select * from cajeros-automaticos top 10"
+		query = "select * from cajeros-automaticos limit 10"
 
 		cursor.execute(query)
 		print("Selecting rows from mobile table using cursor.fetchall")
@@ -20,7 +20,7 @@ def SearchAll():
 			print("asd3  = ", row[2], "\n")
 
 	except (Exception, psycopg2.Error) as error:
-		print("Error while fetching data from PostgreSQL", error)
+		print("[DAO][SearchALL] Error while fetching data from PostgreSQL", error)
 
 	finally:
 		# closing database connection.
